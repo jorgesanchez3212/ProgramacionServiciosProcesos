4dc8cfd49ce3070c32e8ed63ae532ce189ae1a74
Jorge <jsanchezberrocoso@gmail.com>
2023-05-09 13:31:26 +0200

    Ejercicio Taller Con Channels

--- EjercicioTallerChannel/src/main/kotlin/Main.kt
+++ EjercicioTallerChannel/src/main/kotlin/Main.kt
@@ -1,20 +1,40 @@
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
+import java.util.concurrent.BlockingQueue
+import java.util.concurrent.LinkedBlockingQueue
 import kotlin.random.Random
+
 import kotlin.system.measureTimeMillis
 
 fun main() = runBlocking<Unit> {
-    val scope = CoroutineScope( Dispatchers.Default)
-    val listaMecanicos = listOf<Taller>(Taller("1"), Taller("2"), Taller("3"))
-    val listaCoche = mutableListOf<Coche>()
-
-    listaCoche.addAll(generarCoches())
-    val channel = Channel<Coche>(5)
+    val scope = CoroutineScope(Dispatchers.Default)
+    val listaCoches : BlockingQueue<Coche> = LinkedBlockingQueue<Coche>()
+    val listaMecanicos = listOf<Taller>(Taller("1",listaCoches), Taller("2",listaCoches), Taller("3",listaCoches))
 
 
 
+    println("Ejecución con async y await")
+    measureTimeMillis {
+        var recaudacion = 0
+        listaCoches.addAll(generarCoches())
+        val pendientes = listaMecanicos.mapIndexed { index, mecanico ->
+            scope.async {
+                mecanico.repararCoche()
+            }
+        }
+
+        pendientes.forEach {
+            recaudacion += it.await()
+        }
+
+        println("EL total de la recaudación ha sido de $recaudacion")
+
+    }.also {
+        println("La ejecución ha tardado $it ms")
+    }
 
 }
+
 
 fun generarCoches( ): List<Coche>{
     val listaCoche = mutableListOf<Coche>()
--- EjercicioTallerChannel/src/main/kotlin/Taller.kt
+++ EjercicioTallerChannel/src/main/kotlin/Taller.kt
@@ -1,17 +1,46 @@
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import java.util.concurrent.BlockingDeque
+import java.util.concurrent.BlockingQueue
 import kotlin.random.Random
 
 data class Taller(
-    val nombre: String
+    val nombre: String,
+    val listaCoche : BlockingQueue<Coche>
 ) {
+    val mutex = Mutex()
+    val channel = Channel<Coche>(5)
 
-    suspend fun repararCoche(coche : Coche) : Int {
+
+    suspend fun repararCoche() : Int {
         var recaudacion = 0
-        println("Soy el mecanico $nombre")
-        println("Soy el mecanico $nombre y voy a arreglar el coche $coche")
-        delay(Random.nextLong(300,600))
-        recaudacion += coche.coste
-        return recaudacion
+        var coche : Coche
+        var i = 0
+        println("Soy el mecánico $nombre")
 
+        while (listaCoche.isNotEmpty()){
+                mutex.withLock {
+                    val car = listaCoche.take()
+                    channel.send(car)
+                    coche = channel.receive()
+                }
+            println("Soy el mecánico $nombre y voy a arreglar el coche $coche")
+
+
+            delay(Random.nextLong(300,600))
+            recaudacion += coche.coste
+
+        }
+
+
+
+
+
+        return recaudacion
     }
+
+
 }
+
